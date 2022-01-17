2afa42effe4273fc56b09c3bb4dacfff023c4ceb
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-17 21:57:52 +0900

    Create zip.py

---
+++ dictionary/zip.py
@@ -0,0 +1,4 @@
+keys = ('apple', 'pear', 'peach') #tuple
+vals = (300, 250, 400) #tuple
+result = dict(zip(keys, vals)) #zip(lit) dict(딕셔너리)
+print(result) #{'apple': 300, 'pear': 250, 'peach': 400}
